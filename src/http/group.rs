0d68c674ac99a25a354bf5981088c8af1b7bdeae
qumn <2476573497@qq.com>
2022-11-07 18:56:56 +0800

    add group

--- src/http/group.rs
+++ src/http/group.rs
@@ -1,14 +1,16 @@
-use axum::{Router, Extension, routing::post, Json};
+use axum::{routing::post, Extension, Json, Router};
 
-use crate::auth::{AuthUser, self};
-use crate::{persistent::GroupManage, ApiContext};
+use crate::auth::AuthUser;
 use crate::modles::group::*;
+use crate::{persistent::GroupManage, ApiContext};
 use eChat::err::Result;
 
-pub fn router(ctx: &ApiContext) -> Router{
+pub fn router(ctx: &ApiContext) -> Router {
     let group_manage = GroupManage::new(ctx.db.clone());
     Router::new()
         .route("/api/groups", post(create_group))
+        .route("/api/groups/join", post(join_group))
+        .route("/api/groups/agree", post(agree))
         .layer(Extension(group_manage))
 }
 
@@ -18,11 +20,31 @@ pub async fn create_group(
     Extension(group_manage): Extension<GroupManage>,
 ) -> Result<()> {
     let group = Group {
-        gid: 0,
+        gid: 0, // a placeholder, the real gid will be automatically generated
         owner: auth_user.uid,
         name: create_group.name.clone(),
         create_time: chrono::Local::now().naive_local(),
     };
     group_manage.create_group(group).await?;
+    Ok(())
+}
+
+pub async fn join_group(
+    auth_user: AuthUser,
+    Json(join_group): Json<JoinGroup>,
+    Extension(group_manage): Extension<GroupManage>,
+) -> Result<()> {
+    group_manage.join(auth_user.uid, join_group.gid).await?;
+    Ok(())
+}
+
+pub async fn agree(
+    auth_user: AuthUser,
+    Json(agree_group): Json<AgreeGroup>,
+    Extension(group_manage): Extension<GroupManage>,
+) -> Result<()> {
+    group_manage
+        .agree(auth_user.uid, agree_group.uid, agree_group.gid)
+        .await?;
     Ok(())
 }
--- src/modles/group.rs
+++ src/modles/group.rs
@@ -21,3 +21,14 @@ pub enum GroupStatus {
     Agree = 1,
     Refused = 2,
 }
+
+#[derive(Deserialize)]
+pub struct JoinGroup{
+    pub gid: u64
+}
+
+#[derive(Deserialize)]
+pub struct AgreeGroup {
+    pub gid: u64,
+    pub uid: u64
+}
