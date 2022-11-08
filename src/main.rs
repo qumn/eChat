38e1e45b09842c5ebdb2edd4d2218372c321ebca
qumn <2476573497@qq.com>
2022-11-08 14:26:57 +0800

    add cors config

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
+mod auth;
+mod http;
+mod modles;
+mod persistent;
+
 use std::sync::Arc;
 
-use axum::Extension;
 use dashmap::DashMap;
 use eChat::err::Error;
 use http::api_router;
+use modles::message::Msg;
 use persistent::get_pool;
 use sqlx::MySqlPool;
 use tokio::sync::mpsc::Sender;
-mod auth;
-mod http;
-mod persistent;
-mod modles;
-
-pub struct Msg{
-
-}
+use tower_http::cors::{CorsLayer};
+use tower_http::trace::TraceLayer;
 
 #[derive(Clone)]
 pub struct ApiContext {
     pub db: Arc<MySqlPool>,
-    pub sender_map: DashMap<u64, Sender<Msg>>
+    pub sender_map: DashMap<u64, Sender<Msg>>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenvy::dotenv().unwrap(); // load .env file
-    // Initialize the logger 
+                                // Initialize the logger
     tracing_subscriber::fmt::init();
     //tracing_subscriber::fmt().with_max_level(tracing::Level::TRACE).init();
     let pool = get_pool().await?;
-    let ctx = ApiContext { db: Arc::new(pool), sender_map: DashMap::new() };
-    let app = api_router(&ctx).layer(Extension(ctx));
+    let ctx = ApiContext {
+        db: Arc::new(pool),
+        sender_map: DashMap::new(),
+    };
+    let app = api_router(&ctx)
+        .layer(CorsLayer::permissive())
+        .layer(TraceLayer::new_for_http());
+
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
